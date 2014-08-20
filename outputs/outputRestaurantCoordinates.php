00c381d8c5f2d5865f952e46688c59ece7e28446
bessss <v.bessoltsev@gmail.com>
2014-08-20 16:33:14 +0400

    new output restaurant

--- outputs/outputRestaurantCoordinates.php
+++ outputs/outputRestaurantCoordinates.php
@@ -8,20 +8,20 @@ class outputRestaurantCoordinates
 
   function get_coordinates()
   {
-    $sql = 'SELECT * FROM restaurant JOIN company ON restaurant.id_company = company.id';
+    $sql = 'SELECT * FROM restaurant JOIN company ON restaurant.id_company = company.id AND restaurant.network != 1';
     $result = mysql_query($sql) or die($sql);
     if ( mysql_num_rows($result) > 0 )
     {
       for ( $i = 0; $i < mysql_num_rows($result); ++$i )
       {
-        $this -> json[$i] = array( 'idRestaurant' => mysql_result($result,$i,'id'),'accuracy' => '100','longitude' => mysql_result($result,$i,'coordinateX'),'latitude' => mysql_result($result,$i,'coordinateY'),'css_name' => mysql_result($result,$i,'css_name'),'timeOpen' => mysql_result($result,$i,'timeOpen'),'timeClose' => mysql_result($result,$i,'timeClose'),'contact' => mysql_result($result,$i,'contact'),'adress' => mysql_result($result,$i,'adress'),'shotName' => mysql_result($result,$i,'shotName'),'title' => mysql_result($result,$i,'shotName'), 'latitude' => mysql_result($result,$i,'coordinateX'), 'longitude' => mysql_result($result,$i,'coordinateY') );
+        $this -> json[$i] = array( 'idRestaurant' => mysql_result($result,$i,'id'),'accuracy' => '100','longitude' => mysql_result($result,$i,'coordinateX'),'latitude' => mysql_result($result,$i,'coordinateY'),'cssName' => mysql_result($result,$i,'css_name'),'timeOpen' => mysql_result($result,$i,'timeOpen'),'timeClose' => mysql_result($result,$i,'timeClose'),'contact' => mysql_result($result,$i,'contact'),'adress' => mysql_result($result,$i,'adress'),'shotName' => mysql_result($result,$i,'shotName'),'title' => mysql_result($result,$i,'shotName'), 'latitude' => mysql_result($result,$i,'coordinateX'), 'longitude' => mysql_result($result,$i,'coordinateY') );
       }
       $this -> jsonResult['restaurant'] = $this -> json;
       print( str_replace('\/','/',json_encode($this -> jsonResult)) );
     }
     else
     {
-      print('cancel');
+      print('');
     }
   }
 
